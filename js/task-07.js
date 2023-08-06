d252a82c7f2a667e512574f02f238c3e74e7a452
Agata Warzyszyńska <agata.wlazlak@gmail.com>
2023-08-06 12:49:42 +0200

    js-hw-6-second-version+10

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,10 @@ const inputControlSize = document.querySelector("#font-size-control");
 
 const textSize = document.querySelector("#text");
 
+textSize.style.fontSize = "57px";
+
+console.log(textSize);
 inputControlSize.addEventListener("input", (e) => {
   textSize.style.fontSize = `${e.currentTarget.value}px`;
+  console.log(e.currentTarget.value);
 });
--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numberSquares = document.querySelector("input");
+const boxDiv = document.querySelector("#boxes");
+const createBtn = document.querySelector("button[data-create]");
+const destoryBtn = document.querySelector("button[data-destroy]");
+let a = 0;
+
+numberSquares.addEventListener("input", (e) => {
+  a = e.currentTarget.value;
+});
+const clickHandler = () => createBoxes(a);
+
+createBtn.addEventListener("click", clickHandler);
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const newElement = document.createElement("div");
+    newElement.style.backgroundColor = getRandomHexColor();
+    newElement.style.width = `${30 + i * 10}px`;
+    newElement.style.height = `${30 + i * 10}px`;
+    boxDiv.append(newElement);
+  }
+}
+destoryBtn.addEventListener("click", () => destroyBoxes());
+function destroyBoxes() {
+  console.log(boxDiv.children.length);
+  if (boxDiv.children.length !== 0) {
+    const arrayDiv = boxDiv.children;
+    console.log(arrayDiv);
+    for (let i = 0; (i = arrayDiv.length); i++) {
+      boxDiv.firstChild.remove();
+    }
+  }
+}
